ebff8d9d913e15c52bd95de79012c8a9ce045675
Lev Stambler <levstamb@gmail.com>
2023-01-09 16:35:46 -0500

    data

--- scoring/data_generation.py
+++ scoring/data_generation.py
@@ -1,16 +1,63 @@
 import torch
+from bposd import bposd_decoder
+import numpy as np
+from .. import utils
 
+
+def gen_random_ldpc(n, k, deg_row):
+    iden_left = np.eye(n - k)
+    x = np.zeros((n-k, k))
+    x[:, :deg_row - 1] = 1
+    perm = np.stack([np.random.permutation(k) for _ in range((n - k))])
+    x = np.take(x, perm)
+    # TODO: above is wrong use first approach
+    rand_ldpc_H = np.concatenate((iden_left, x), axis=1)
+    G = utils.HtoG(rand_ldpc_H)
+    return rand_ldpc_H.astype(np.int16), G.astype(np.int16)
+
+
+def sample_noisy_codespace(n, p_failures):
+    pass
+
+
+# TODO: can we parallelize this dramatically? I think yes
+def run_decoder(pc_mat_systematic, n_runs, p_fails):
+    n = pc_mat_systematic.shape[1]
+    rho = p_fails
+    bpd = bposd_decoder(pc_mat_systematic, channel_probs=rho,
+                        bp_method="product_sum", osd_method="osd_e", osd_order=3, max_iter=5)
+    n_succ = 0
+    for i in range(n_runs):
+        noise = sample_noisy_codespace(n, rho)
+        synd = (pc_mat_systematic @ noise) % 2
+        if sum(synd) == 0:
+            n_succ += 1
+        else:
+            eded = bpd.decode(synd)
+            eq = np.array_equal(eded, noise)
+            n_succ += eq
+
+    return n_succ
 
 class ScoringInitDataset(torch.utils.data.Dataset):
     """Some Information about MyDataset"""
-    def __init__(self, error_prob_sample: function, random_code_sample: function, n_samples):
+
+    def __init__(self, error_prob_sample: function, random_code_sample: function, dataset_size, item_sample_size=10_000):
         self.error_prob = error_prob_sample
         self.random_code = random_code_sample
-        self.n_samples = n_samples
+        self.dataset_size = dataset_size
         super(ScoringInitDataset, self).__init__()
 
     def __getitem__(self, index):
-        return (self.random_code(), self.error_prob())
+        H, _ = self.random_code()
+        e = self.error_prob()
+        error_rate = self.calculate_error_rate(H, e)
+        sample = {'code': H, 'error_probs': e, 'frame_error_rate': error_rate}
+
+        return sample
+    
+    def calculate_error_rate(self, code, error_prob):
+        return run_decoder(code, self.item_sample_size, error_prob) / self.item_sample_size
 
     def __len__(self):
-        return self.n_samples
+        return self.dataset_size
--- utils.py
+++ utils.py
@@ -1,19 +1,102 @@
 import numpy as np
 import torch
 
+
 def sign_to_bin(x):
     return 0.5 * (1 - x)
+
 
 def bin_to_sign(x):
     return 1 - 2 * x
 
+
 def EbN0_to_std(EbN0, rate):
-    snr =  EbN0 + 10. * np.log10(2 * rate)
+    snr = EbN0 + 10. * np.log10(2 * rate)
     return np.sqrt(1. / (10. ** (snr / 10.)))
+
 
 def BER(x_pred, x_gt):
     return torch.mean((x_pred != x_gt).float()).item()
 
+
 def FER(x_pred, x_gt):
     return torch.mean(torch.any(x_pred != x_gt, dim=1).float()).item()
 
+# Thanks to https://github.com/christiansiegel/coding-theory-algorithms/blob/master/LinearBlockCode.py
+
+
+def GtoP(G):
+    """Extract the submatrix P from a Generator Matrix in systematic form.
+    Args:
+        G: Generator Matrix in systematic form
+    Returns:
+        Submatrix P of G.
+    """
+    k = G.shape[0]
+    n = G.shape[1]
+    P = G[:k, :n - k]
+    return P
+
+
+def HtoSystematicH(H):
+    """Convert a parity check matrix into systematic form,
+    gotten from https://gist.github.com/popcornell/bc29d1b7ba37d824335ab7b6280f7fec"""
+    m, n = H.shape
+
+    i = 0
+    j = 0
+
+    while i < m and j < n:
+        # find value and index of largest element in remainder of column j
+        k = np.argmax(H[i:, j]) + i
+
+        # swap rows
+        # M[[k, i]] = M[[i, k]] this doesn't work with numba
+        temp = np.copy(H[k])
+        H[k] = H[i]
+        H[i] = temp
+
+        aijn = H[i, j:]
+
+        # make a copy otherwise M will be directly affected
+        col = np.copy(H[:, j])
+
+        col[i] = 0  # avoid xoring pivot row with itself
+
+        flip = np.outer(col, aijn)
+
+        H[:, j:] = H[:, j:] ^ flip
+
+        i += 1
+        j += 1
+
+    return H
+
+
+def HtoP(H):
+    """Extract the submatrix P from a Parity Check Matrix in systematic form.
+    Args:
+        H: Parity Check Matrix in systematic form
+    Returns:
+        Submatrix P of G.
+    """
+    n = H.shape[1]
+    k = n - H.shape[0]
+    PK = H[:, n - k:n]
+    P = PK.transpose(0, 1)
+    return P
+
+
+def HtoG(H):
+    """Convert a Parity Check Matrix in systematic form to a Generator Matrix.
+    Args:
+        H: Parity Check Matrix in systematic form
+    Returns:
+        Generator Matrix G
+    """
+    n = H.shape[1]
+    k = n - H.shape[0]
+    P = HtoP(H)
+    Ik = np.eye(k)
+    G = np.concatenate((P, Ik), axis=0)
+    return G
