ee6431e781b5e963e713d76d67108102a13952c7
Lev Stambler <levstamb@gmail.com>
2023-01-10 18:23:40 -0500

    venv

--- scoring/data_generation.py
+++ scoring/data_generation.py
@@ -61,3 +61,16 @@ class ScoringInitDataset(torch.utils.data.Dataset):
 
     def __len__(self):
         return self.dataset_size
+
+if __name__ == "__main__":
+    n = 40
+    k = 10
+    deg_row = 5
+    p_error = 0.05
+    def ldpc_sample():
+        return gen_random_ldpc(n, k, deg_row)
+    def error_sample():
+       return (np.random.rand(n) <= p_error).astype(np.uint16)
+    d = ScoringInitDataset(error_sample, ldpc_sample, 1_000)
+    for i in range(10):
+        print(f"Sample {i + 1}: {d.__getitem__(i)}")
