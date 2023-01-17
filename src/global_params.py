70d8288e0d013465e5c67f0a364dda1dc8e4df20
Lev Stambler <levstamb@gmail.com>
2023-01-17 14:35:11 -0500

    params

--- src/global_params.py
+++ src/global_params.py
@@ -1,23 +1,23 @@
 params = {
-	'n_data_qubits': 30,
-	'n_check_qubits': 20,
+	'n_data_qubits': 90,
+	'n_check_qubits': 70,
 	'deg_bit_lower': 2,
 	'deg_phase_lower': 2,
 	'deg_check_to_check_lower': 2,
-	'deg_bit_upper': 6,
-	'deg_phase_upper': 6,
-	'deg_check_to_check_upper': 6,
+	'deg_bit_upper': 8,
+	'deg_phase_upper': 8,
+	'deg_check_to_check_upper': 8,
 	# TODO: delete the below
-	'constant_error_rate_lower': 0.009, # lower bounding here is tricky as we need stat sig
-	'constant_error_rate_upper': 0.05,
+	'constant_error_rate_lower': 0.05, # lower bounding here is tricky as we need stat sig
+	'constant_error_rate_upper': 0.1,
 	'scoring_model_save_path': 'best_scoring_model',
 	'n_genetic_epochs': 3,
 	'n_score_testing_samples': 500,
 	'n_score_training_per_epoch_initial': 1_000,
-	'n_score_training_per_epoch_genetic': 400,
-	'n_score_epochs': 2,
+	'n_score_training_per_epoch_genetic': 500,
+	'n_score_epochs': 3,
 	# The number of times to run a decoder to generate the error rate
-	'n_decoder_rounds': 10_000
+	'n_decoder_rounds': 50_000
 }
 
 # TODO: add in # optimize steps here...
