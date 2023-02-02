7a84ebbc381814f6181a0b2b20d034d85abe3819
Lev Stambler <levstamb@gmail.com>
2023-02-02 17:16:16 -0500

    progress and actually a large jump

--- src/global_params.py
+++ src/global_params.py
@@ -1,5 +1,5 @@
 params = {
-    'n_data_qubits': 30,
+    'n_data_qubits': 31,
     'n_check_qubits': 20,
     'deg_bit_lower': 2,
     'deg_phase_lower': 2,
@@ -8,11 +8,11 @@ params = {
     'deg_phase_upper': 6,
     'deg_check_to_check_upper': 6,
     # lower bounding here is tricky as we need stat sig
-    'constant_error_rate_lower': 0.005,
-    'constant_error_rate_upper': 0.01,
+    'constant_error_rate_lower': 0.02,
+    'constant_error_rate_upper': 0.02,
     'n_genetic_epochs': 10,
     'n_score_testing_samples': 50,
-    'n_score_training_per_epoch_initial': 500,  # 10_000,
+    'n_score_training_per_epoch_initial': 1000,  # 10_000,
     'n_score_training_per_epoch_genetic': 500,
     'n_score_epochs': 5,
     # The number of times to run a decoder to generate the error rate
