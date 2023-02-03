0d5495a3a9b49f320c232a29f47ae976c2be6b95
Lev Stambler <levstamb@gmail.com>
2023-02-03 15:38:11 -0500

    resutls

--- src/global_params.py
+++ src/global_params.py
@@ -19,5 +19,5 @@ params = {
     'n_decoder_rounds': 100_000,
     'p_skip_mutation': 0.8,
     'p_random_mutation': 0.0005,
-    'eval_p_range': [0.001, 0.005, 0.01, 0.015]
+    'eval_p_range': [0.001, 0.005, 0.01, 0.02]
 }
