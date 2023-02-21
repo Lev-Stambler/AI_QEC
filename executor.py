d7b5916a5082d0d26dc1c880d9931174789023d9
Lev Stambler <levstamb@gmail.com>
2023-02-20 19:05:38 -0500

    more data

--- executor.py
+++ executor.py
@@ -2,5 +2,5 @@ import os
 import sys
 print("Starting run")
 sys.path.append(os.getcwd() + "/src")
-from training import main
+from training_qec import main
 model = main(None, load_saved_scoring_model=False)
