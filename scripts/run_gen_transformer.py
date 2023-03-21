2f45d38353f9f37594d5356e69ab6715b2b988c5
Lev Stambler <levstamb@gmail.com>
2023-03-21 08:48:19 +0000

    a

--- scripts/run_gen_transformer.py
+++ scripts/run_gen_transformer.py
@@ -1,6 +1,6 @@
-from training_qec import main
 import os
 import sys
 sys.path.append(os.getcwd() + "../src")
+from training_qec import main
 model = main(None, load_saved_scoring_model=False,
              skip_initialization_training=False, skip_eval=True, initialize_epoch_start=1, genetic_epoch_start=1)
