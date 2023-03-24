9941befa8d5084b139d14d3b2bc0a406dddf246f
Lev Stambler <levstamb@gmail.com>
2023-03-24 11:20:18 +0000

    axis

--- src/CPC/cpc_code.py
+++ src/CPC/cpc_code.py
@@ -7,5 +7,5 @@ def get_classical_code_cpc(bit_adj, phase_adj, check_adj) -> npt.NDArray:
         classical codes
     """
             # The yellow nodes in the paper
-    pc = np.concatenate([phase_adj.transpose(), ((bit_adj.transpose() @ phase_adj) % 2) ^ check_adj, bit_adj.transpose(), np.eye(check_adj.shape[-1])], axis=-1)
+    pc = np.concatenate([phase_adj.transpose(), (((bit_adj.transpose() @ phase_adj) % 2) + check_adj) % 2, bit_adj.transpose(), np.eye(check_adj.shape[-1])], axis=-1)
     return np.array(pc, dtype=np.uint8)
